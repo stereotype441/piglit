d90b42c74808f25449b44352cc16e938a6075273
Paul Berry <stereotype441@gmail.com>
2011-11-15 12:08:58 -0800

    WIP

---
+++ tests/shaders/vertex-cache.cpp
@@ -0,0 +1,113 @@
+/*
+ * Copyright © 2011 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+compile_vs(unsigned num_attributes, unsigned num_varyings)
+{
+	char tmp[1024];
+	vs_text += "#version 130\n";
+	sprintf(tmp, "#define NUM_ATTRIBUTES %u\n", num_attributes);
+	vs_text += tmp;
+	sprintf(tmp, "#define NUM_VARYINGS %u\n", num_varyings);
+	vs_text += tmp;
+	vs_text +=
+		"\n"
+		"uniform uvec4 attribute_multipliers[NUM_ATTRIBUTES];\n"
+		"uniform uvec4 attribute_offsets[NUM_ATTRIBUTES];\n"
+		"uniform uvec4 varying_multipliers[NUM_VARYINGS];\n"
+		"uniform uvec4 varying_offsets[NUM_VARYINGS];\n"
+	for (unsigned i = 0; i < num_attributes; ++i) {
+		sprintf(tmp, "in uvec4 attribute_%u;\n", i);
+		vs_text += tmp;
+	}
+	vs_text +=
+		"flat out uvec4 varyings[NUM_VARYINGS];\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"  uint grid_index = attribute_0.x;\n"
+		"  uvec2 grid_coords = uvec2(grid_index % 65536u,\n"
+		"                            grid_index / 65536u);\n"
+		"  gl_Position = vec4(grid_coords - grid_offset, 0.0,\n"
+		"                     grid_scale);\n"
+		"\n"
+		"  bool attributes_ok = true;\n";
+	for (unsigned i = 0; i < num_attributes; ++i) {
+		sprintf(tmp,
+			"  if (attribute_%u != "
+			"attribute_multipliers[%u] * grid_index + "
+			"attribute_offsets[%u]) {\n",
+			i, i, i);
+		vs_text += tmp;
+		vs_text +=
+			"    attributes_ok = false;\n"
+			"  }\n";
+	}
+	vs_text +=
+		"  if (attributes_ok) {\n"
+		"    for (int i = 0; i < NUM_VARYINGS; ++i) {\n"
+		"      varyings[i] = varying_multipliers[i] * grid_index +"
+		"                    varying_offsets[i];\n"
+		"    }\n"
+		"  } else {\n"
+		"    for (int i = 0; i < NUM_VARYINGS; ++i {\n"
+		"      varyings[i] = uvec4(0u);\n"
+		"    }\n"
+		"  }\n"
+		"}\n";
+}
+
+compile_fs(unsigned num_varyings)
+{
+	char tmp[1024];
+	fs_text += "#version 130\n";
+	sprintf(tmp, "#define NUM_VARYINGS %u\n", num_varyings);
+	fs_text += tmp;
+	fs_text +=
+		"\n"
+		"uniform uvec4 varying_multipliers[NUM_VARYINGS];\n"
+		"uniform uvec4 varying_offsets[NUM_VARYINGS];\n"
+		"flat in uvec4 varyings[NUM_VARYINGS];\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"  uint grid_index = varyings[0].x;\n"
+		"  uvec2 grid_coords = uvec2(grid_index % 65536u,\n"
+		"                            grid_index / 65536u);\n"
+		"  uvec2 expected_grid_coords = uvec2(gl_FragCoord.xy) /\n"
+		"                               grid_size;\n"
+		"\n"
+		"  bool varyings_ok = true;\n"
+		"  for (int i = 0; i < NUM_VARYINGS; ++i) {\n"
+		"    if (varyings[i] !=\n"
+		"        varying_multipliers[i] * grid_index +\n"
+		"        varying_offsets[i]) {\n"
+		"      varyings_ok = false;\n"
+		"    }\n"
+		"  }\n"
+		"  if (varyings_ok) {\n"
+		"    gl_FragColor = vec4(0.0, 1.0, 0.0, 1.0);\n"
+		"  } else {\n"
+		"    gl_FragColor = vec4(1.0, 0.0, 0.0, 1.0);\n"
+		"  }\n"
+		"}\n";
+}
