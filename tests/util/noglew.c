b79751cad65ab262820aba684642a2168e13c2a4
Paul Berry <stereotype441@gmail.com>
2012-01-06 15:57:38 -0800

    Initial noglew.c implementation

--- tests/util/noglew.c
+++ tests/util/noglew.c
@@ -1,1 +1,38 @@
-// FOO
+#if defined(_WIN32)
+#include <windows.h>
+#endif
+
+#include <assert.h>
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+
+#include "config.h"
+#if defined(HAVE_SYS_TIME_H) && defined(HAVE_SYS_RESOURCE_H) && defined(HAVE_SETRLIMIT)
+#include <sys/time.h>
+#include <sys/resource.h>
+#define USE_SETRLIMIT
+#endif
+
+#include "piglit-util.h"
+#include "glxew.h"
+
+// TODO: it seems bad to call this "glGetProcAddress".
+#if defined(_WIN32)
+#  define glGetProcAddress(name) wglGetProcAddress((LPCSTR)name)
+#else
+#  if defined(__APPLE__)
+#    define glGetProcAddress(name) NSGLGetProcAddress(name)
+#  else
+#    if defined(__sgi) || defined(__sun)
+#      define glGetProcAddress(name) dlGetProcAddress(name)
+#    else /* __linux */
+#      define glGetProcAddress(name) (*glXGetProcAddressARB)(name)
+#    endif
+#  endif
+#endif
+
+#include "wrappers.c"
+
