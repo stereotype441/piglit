bbd6b0df6237363c121c17d44176aa8b2e0d6545
Paul Berry <stereotype441@gmail.com>
2012-02-27 10:33:34 -0800

    Get rid of unnecessary workarounds from piglit-dispatch.h

--- tests/util/piglit-dispatch.h
+++ tests/util/piglit-dispatch.h
@@ -90,21 +90,6 @@ typedef unsigned int GLhandleARB;
 #define GLEW_OES_compressed_paletted_texture (piglit_is_extension_supported("OES_compressed_paletted_texture"))
 #define glewInit() do { } while (0)
 
-// Why is this necessary?
-#define GL_COMPRESSED_LUMINANCE_ALPHA_3DC_ATI 0x8837
-#define GL_CLIP_PLANE6 0x3006
-#define GL_CLIP_PLANE7 0x3007
-#define GL_PALETTE4_RGB8_OES 0x8B90
-#define GL_PALETTE4_RGBA8_OES 0x8B91
-#define GL_PALETTE4_R5_G6_B5_OES 0x8B92
-#define GL_PALETTE4_RGBA4_OES 0x8B93
-#define GL_PALETTE4_RGB5_A1_OES 0x8B94
-#define GL_PALETTE8_RGB8_OES 0x8B95
-#define GL_PALETTE8_RGBA8_OES 0x8B96
-#define GL_PALETTE8_R5_G6_B5_OES 0x8B97
-#define GL_PALETTE8_RGBA4_OES 0x8B98
-#define GL_PALETTE8_RGB5_A1_OES 0x8B99
-
 // Prevent redundant stuff from being defined.  TODO: autogen this.
 #define GL_ARB_framebuffer_object
 #define GL_ARB_geometry_shader4
