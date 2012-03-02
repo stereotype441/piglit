3df6bc509e8eee186183cb157db98e1fe5d38ed7
Paul Berry <stereotype441@gmail.com>
2012-03-01 16:55:18 -0800

    piglit-dispatch: Remaining infrastructure
    Previous patches introduced code generation for the auto-generated
    portions of the piglit-dispatch mechanism.  This patch supplies the
    remaining hand-written code, which includes:
    
    - Code to initialize piglit-dispatch.
    
    - The actions to be taken when looking up a function, when function
      lookup fails, or when a function is unsupported.
    
    - Code to determine whether piglit-dispatch has been initialized,
      which GL version is supported, and whether a given extension is
      supported.
    
    - Definitions of basic GL types.

---
+++ tests/util/piglit-dispatch.h
@@ -0,0 +1,106 @@
+/* Copyright 2012 Intel Corporation
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a
+ * copy of this software and associated documentation files (the "Software"),
+ * to deal in the Software without restriction, including without limitation
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice (including the next
+ * paragraph) shall be included in all copies or substantial portions of the
+ * Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.  IN NO EVENT SHALL
+ * THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+ * IN THE SOFTWARE.
+ */
+
+/** \file piglit-dispatch.h
+ *
+ * Dispatch mechanism allowing a single set of piglit tests to select
+ * at run time whether to use OpenGL or GLES.
+ */
+
+#ifndef __piglit_dispatch_h__
+#define __piglit_dispatch_h__
+
+#include <stdint.h>
+#include <stddef.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+typedef unsigned int GLenum;
+typedef unsigned int GLbitfield;
+typedef unsigned int GLuint;
+typedef int GLint;
+typedef int GLsizei;
+typedef unsigned char GLboolean;
+typedef signed char GLbyte;
+typedef short GLshort;
+typedef unsigned char GLubyte;
+typedef unsigned short GLushort;
+typedef unsigned long GLulong;
+typedef float GLfloat;
+typedef float GLclampf;
+typedef double GLdouble;
+typedef double GLclampd;
+typedef void GLvoid;
+typedef int64_t GLint64EXT;
+typedef uint64_t GLuint64EXT;
+typedef GLint64EXT  GLint64;
+typedef GLuint64EXT GLuint64;
+typedef struct __GLsync *GLsync;
+
+typedef char GLchar;
+
+typedef ptrdiff_t GLintptr;
+typedef ptrdiff_t GLsizeiptr;
+
+typedef ptrdiff_t GLintptrARB;
+typedef ptrdiff_t GLsizeiptrARB;
+
+typedef char GLcharARB;
+typedef unsigned int GLhandleARB;
+
+typedef void (*GLDEBUGPROCARB)(GLenum source,GLenum type,GLuint id,GLenum severity,GLsizei length,const GLchar *message,GLvoid *userParam);
+
+typedef void piglit_dispatch_function(void);
+
+typedef piglit_dispatch_function *piglit_get_proc_address_function(const char *);
+
+typedef void piglit_error_function(const char *);
+
+typedef enum {
+	PIGLIT_DISPATCH_GL,
+	PIGLIT_DISPATCH_GL_FWD,
+	PIGLIT_DISPATCH_ES1,
+	PIGLIT_DISPATCH_ES2
+} piglit_dispatch_api;
+
+void piglit_dispatch_init(piglit_dispatch_api api,
+			  piglit_get_proc_address_function *get_core_proc,
+			  piglit_get_proc_address_function *get_ext_proc,
+			  piglit_error_function *unsupported_proc,
+			  piglit_error_function *failure_proc);
+
+#include "generated_dispatch.h"
+
+void piglit_dispatch_default_init();
+
+/* Prevent gl.h from being included, since it will attempt to define
+ * the functions we've already defined.
+ */
+#define __gl_h_
+
+#ifdef __cplusplus
+} /* end extern "C" */
+#endif
+
+#endif /* __piglit_dispatch_h__ */
