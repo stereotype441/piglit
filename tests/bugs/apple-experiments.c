faf2bcb304f34715349a9abe904113761c6f591b
Paul Berry <stereotype441@gmail.com>
2012-03-20 10:47:57 -0700

    wip

---
+++ tests/bugs/apple-experiments.c
@@ -0,0 +1,41 @@
+#include "piglit-util.h"
+
+int piglit_width = 100, piglit_height = 100;
+int piglit_window_mode = GLUT_RGB | GLUT_DOUBLE;
+
+void piglit_init(int argc, char **argv)
+{
+	int i;
+	GLuint prog, vshader, fshader;
+	GLsizei count;
+	GLuint shaders[16];
+	GLhandleARB shadersARB[16];
+	prog = glCreateProgram();
+	printf("glCreateProgram() => %d\n", prog);
+	vshader = glCreateShader(GL_VERTEX_SHADER);
+	printf("glCreateShader(GL_VERTEX_SHADER) => %d\n", vshader);
+	fshader = glCreateShader(GL_FRAGMENT_SHADER);
+	printf("glCreateShader(GL_FRAGMENT_SHADER) => %d\n", fshader);
+	glAttachShader(prog, vshader);
+	printf("glAttachShader(%d, %d)\n", prog, vshader);
+	glAttachShader(prog, fshader);
+	printf("glAttachShader(%d, %d)\n", prog, fshader);
+	glGetAttachedShaders(prog, 8, &count, shaders);
+	printf("glGetAttachedShaders(%d, 8, &count, shaders):\n", prog);
+	printf("  count = %d\n", count);
+	for (i = 0; i < 16; ++i) {
+		printf("  shaders[%d] = %d\n", i, shaders[i]);
+	}
+	glGetAttachedObjectsARB(prog, 8, &count, shadersARB);
+	printf("glGetAttachedShadersARB(%d, 8, &count, shadersARB):\n", prog);
+	printf("  count = %d\n", count);
+	for (i = 0; i < 16; ++i) {
+		printf("  shadersARB[%d] = %ld\n", i, (unsigned long) shadersARB[i]);
+	}
+}
+
+enum piglit_result
+piglit_display(void)
+{
+	return PIGLIT_PASS;
+}
