3998ff366f04342926b162b35ffea758ccd51fde
Paul Berry <stereotype441@gmail.com>
2012-03-26 14:03:31 -0700

    Msaa test function

---
+++ tests/bugs/msaa.c
@@ -0,0 +1,49 @@
+#include "piglit-util.h"
+
+int piglit_width = 100, piglit_height = 100;
+int piglit_window_mode = GLUT_RGBA | GLUT_DOUBLE;
+
+static const char *vert =
+	"#version 120\n"
+	"void main()\n"
+	"{\n"
+	"  gl_Position = gl_Vertex;\n"
+	"}\n";
+
+static const char *frag =
+	"#version 120\n"
+	"void main()\n"
+	"{\n"
+	"  gl_FragColor = vec4(0.5);\n"
+	"}\n";
+
+enum piglit_result
+piglit_display(void)
+{
+	float vertices[][2] = {
+		{ -1, 1 },
+		{ 0, 1 },
+		{ -1, 0 }
+	};
+
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glVertexPointer(2, GL_FLOAT, sizeof(vertices[0]), &vertices);
+	glEnableClientState(GL_VERTEX_ARRAY);
+       	glDrawArrays(GL_TRIANGLES, 0, 3);
+	piglit_present_results();
+
+	return PIGLIT_PASS;
+}
+
+void
+piglit_init(int argc, char **argv)
+{
+	int vs, fs, prog;
+
+	piglit_require_GLSL_version(120);
+	vs = piglit_compile_shader_text(GL_VERTEX_SHADER, vert);
+	fs = piglit_compile_shader_text(GL_FRAGMENT_SHADER, frag);
+	prog = piglit_link_simple_program(vs, fs);
+	piglit_UseProgram(prog);
+}
