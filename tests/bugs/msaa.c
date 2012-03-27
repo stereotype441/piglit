b922c947118c116fdfae7d0b14139e5a15389ffb
Paul Berry <stereotype441@gmail.com>
2012-03-27 11:26:50 -0700

    MSAA test using framebuffer

--- tests/bugs/msaa.c
+++ tests/bugs/msaa.c
@@ -40,10 +40,23 @@ void
 piglit_init(int argc, char **argv)
 {
 	int vs, fs, prog;
+	GLuint fbo, rb;
 
 	piglit_require_GLSL_version(120);
 	vs = piglit_compile_shader_text(GL_VERTEX_SHADER, vert);
 	fs = piglit_compile_shader_text(GL_FRAGMENT_SHADER, frag);
 	prog = piglit_link_simple_program(vs, fs);
 	piglit_UseProgram(prog);
+
+	glGenFramebuffers(1, &fbo);
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fbo);
+	glBindFramebuffer(GL_READ_FRAMEBUFFER, fbo);
+	glGenRenderbuffers(1, &rb);
+	glBindRenderbuffer(GL_RENDERBUFFER, rb);
+	glRenderbufferStorageMultisample(GL_RENDERBUFFER, 4, GL_RGBA, 100, 100);
+	glFramebufferRenderbuffer(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0, GL_RENDERBUFFER, rb);
+	if (glCheckFramebufferStatus(GL_DRAW_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+		printf("OMG! Framebuffer not complete!\n");
+		piglit_report_result(PIGLIT_FAIL);
+	}
 }
