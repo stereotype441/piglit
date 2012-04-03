effd86bbcdeb17d81e6777f8dfc4dacd782dc450
Paul Berry <stereotype441@gmail.com>
2012-04-03 12:43:06 -0700

    Render a complex pattern

--- tests/bugs/msaa.c
+++ tests/bugs/msaa.c
@@ -1,6 +1,7 @@
 #include "piglit-util.h"
 
 #define SIZE 4
+#define SIZE2 (2*SIZE)
 
 int piglit_width = SIZE, piglit_height = SIZE;
 int piglit_window_mode = GLUT_RGBA | GLUT_DOUBLE;
@@ -10,29 +11,54 @@ static const char *vert =
 	"void main()\n"
 	"{\n"
 	"  gl_Position = gl_Vertex;\n"
+	"  gl_FrontColor = gl_Color;\n"
 	"}\n";
 
 static const char *frag =
 	"#version 120\n"
 	"void main()\n"
 	"{\n"
-	"  gl_FragColor = vec4(0.5);\n"
+	"  gl_FragColor = gl_Color;\n"
 	"}\n";
 
 enum piglit_result
 piglit_display(void)
 {
-	float vertices[][2] = {
-		{ -1, 1 },
-		{ 1, 1 },
-		{ -1, -1 }
-	};
+	float vertices[SIZE2][SIZE2][6][2];
+	float colors[SIZE2][SIZE2][6][4];
+	int i, j, v;
 
-	glClearColor(0.0, 0.0, 0.0, 0.0);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glVertexPointer(2, GL_FLOAT, sizeof(vertices[0]), &vertices);
+	for (i = 0; i < SIZE2; ++i) {
+		for (j = 0; j < SIZE2; ++j) {
+			float x0 = (2.0*i-SIZE2)/SIZE2;
+			float x1 = (2.0*(i+1)-SIZE2)/SIZE2;
+			float y0 = (2.0*j-SIZE2)/SIZE2;
+			float y1 = (2.0*(j+1)-SIZE2)/SIZE2;
+			vertices[i][j][0][0] = x0; vertices[i][j][0][1] = y0;
+			vertices[i][j][1][0] = x1; vertices[i][j][1][1] = y0;
+			vertices[i][j][2][0] = x0; vertices[i][j][2][1] = y1;
+			vertices[i][j][3][0] = x0; vertices[i][j][3][1] = y1;
+			vertices[i][j][4][0] = x1; vertices[i][j][4][1] = y0;
+			vertices[i][j][5][0] = x1; vertices[i][j][5][1] = y1;
+			for (v = 0; v < 6; ++v) {
+				colors[i][j][v][0] = i/15.0;
+				colors[i][j][v][1] = j/15.0;
+				colors[i][j][v][2] = 0.0;
+				colors[i][j][v][3] = 1.0;
+			}
+		}
+	}
+
+	//	glClearColor(0.0, 0.0, 0.0, 0.0);
+	//	glClear(GL_COLOR_BUFFER_BIT);
+	glVertexPointer(2, GL_FLOAT, sizeof(vertices[0][0][0]), &vertices);
 	glEnableClientState(GL_VERTEX_ARRAY);
-       	glDrawArrays(GL_TRIANGLES, 0, 3);
+	glColorPointer(4, GL_FLOAT, sizeof(colors[0][0][0]), &colors);
+	glEnableClientState(GL_COLOR_ARRAY);
+       	glDrawArrays(GL_TRIANGLES, 0, 6*SIZE2*SIZE2);
+
+	piglit_print_readpixels(SIZE, SIZE, GL_RGBA, GL_UNSIGNED_BYTE);
+
 	piglit_present_results();
 
 	return PIGLIT_PASS;
