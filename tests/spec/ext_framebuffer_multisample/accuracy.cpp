565748d3cd19e5624c4d922f2e2c5933fc3393a8
Paul Berry <stereotype441@gmail.com>
2012-04-04 08:05:49 -0700

    Get multisample test to actually render properly

--- tests/spec/ext_framebuffer_multisample/accuracy.cpp
+++ tests/spec/ext_framebuffer_multisample/accuracy.cpp
@@ -23,35 +23,42 @@
 
 #include "piglit-util.h"
 
-int piglit_width = 256, piglit_height = 256;
-int piglit_window_mode = GLUT_RGBA | GLUT_DOUBLE;
-
 const int NUM_HORIZ_TILES = 8;
 const int NUM_VERT_TILES = 8;
 const int NUM_TOTAL_TILES = NUM_HORIZ_TILES * NUM_VERT_TILES;
-const float TILE_SIZE = 0.25;
+const int TILE_SIZE = 32;
+
+int piglit_width = TILE_SIZE * NUM_HORIZ_TILES;
+int piglit_height = TILE_SIZE * NUM_VERT_TILES;
+int piglit_window_mode = GLUT_RGBA | GLUT_ALPHA | GLUT_DOUBLE;
+
+GLuint fbo;
 
 class Tile
 {
 public:
 	Tile(int tile_num);
-	void populate_vertices(float (&vertices)[4][2], float x_offset, float y_offset);
+	void populate_vertices(float (&vertices)[4][2]);
 
 private:
+	int x_tile;
+	int y_tile;
 	float v0;
 	float v1;
 	bool rotated;
 };
 
 Tile::Tile(int tile_num)
-	: v0(float(tile_num % (NUM_TOTAL_TILES/2)) / (NUM_TOTAL_TILES/2)),
+	: x_tile(tile_num % NUM_HORIZ_TILES),
+	  y_tile(NUM_VERT_TILES - 1 - tile_num / NUM_HORIZ_TILES),
+	  v0(float(tile_num % (NUM_TOTAL_TILES/2)) / (NUM_TOTAL_TILES/2)),
 	  v1(1.0 - v0),
 	  rotated(tile_num >= NUM_TOTAL_TILES/2)
 {
 }
 
 void
-Tile::populate_vertices(float (&vertices)[4][2], float x_offset, float y_offset)
+Tile::populate_vertices(float (&vertices)[4][2])
 {
 	/* Compute quad coordinates in uv space */
 	float quad[4][2] = {
@@ -71,10 +78,14 @@ Tile::populate_vertices(float (&vertices)[4][2], float x_offset, float y_offset)
 		}
 	}
 
-	/* Scale down to TILE_SIZE and apply offset */
+	/* Scale down to tile size and apply offset */
+	float x_offset = float(2*x_tile) / NUM_HORIZ_TILES - 1.0;
+	float y_offset = float(2*y_tile) / NUM_VERT_TILES - 1.0;
 	for (int i = 0; i < 4; ++i) {
-		vertices[i][0] = x_offset + quad[i][0] * TILE_SIZE;
-		vertices[i][1] = y_offset + quad[i][1] * TILE_SIZE;
+		vertices[i][0] =
+			x_offset + quad[i][0] * (2.0 / NUM_HORIZ_TILES);
+		vertices[i][1] =
+			y_offset + quad[i][1] * (2.0 / NUM_VERT_TILES);
 	}
 }
 
@@ -85,11 +96,7 @@ draw_pattern(void)
 	unsigned int indices[NUM_TOTAL_TILES][6];
 
 	for (int i = 0; i < NUM_TOTAL_TILES; ++i) {
-		int x_tile = i % NUM_HORIZ_TILES;
-		int y_tile = NUM_VERT_TILES - 1 - i / NUM_HORIZ_TILES;
-		float x_offset = float(2*x_tile) / NUM_HORIZ_TILES - 1.0;
-		float y_offset = float(2*y_tile) / NUM_VERT_TILES - 1.0;
-		Tile(i).populate_vertices(vertices[i], x_offset, y_offset);
+		Tile(i).populate_vertices(vertices[i]);
 		indices[i][0] = 4*i;
 		indices[i][1] = 4*i + 1;
 		indices[i][2] = 4*i + 2;
@@ -107,7 +114,12 @@ draw_pattern(void)
 enum piglit_result
 piglit_display(void)
 {
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fbo);
 	draw_pattern();
+	glBindFramebuffer(GL_READ_FRAMEBUFFER, fbo);
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+	glBlitFramebuffer(0, 0, 256, 256, 0, 0, piglit_width, piglit_height,
+			  GL_COLOR_BUFFER_BIT, GL_NEAREST);
 
 	piglit_present_results();
 
@@ -117,4 +129,20 @@ piglit_display(void)
 void
 piglit_init(int argc, char **argv)
 {
+	glGenFramebuffers(1, &fbo);
+	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, fbo);
+	glBindFramebuffer(GL_READ_FRAMEBUFFER, fbo);
+
+	GLuint rb;
+	glGenRenderbuffers(1, &rb);
+	glBindRenderbuffer(GL_RENDERBUFFER, rb);
+	glRenderbufferStorageMultisample(GL_RENDERBUFFER, 4, GL_RGBA,
+					 256, 256);
+	glFramebufferRenderbuffer(GL_DRAW_FRAMEBUFFER, GL_COLOR_ATTACHMENT0,
+				  GL_RENDERBUFFER, rb);
+
+	if (glCheckFramebufferStatus(GL_DRAW_FRAMEBUFFER) != GL_FRAMEBUFFER_COMPLETE) {
+		printf("Framebuffer not complete\n");
+		piglit_report_result(PIGLIT_FAIL);
+	}
 }
