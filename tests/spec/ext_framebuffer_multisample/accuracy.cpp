f1c7712a0d2900f4ac303563ddc8657b48b56a2b
Paul Berry <stereotype441@gmail.com>
2012-04-10 04:38:02 -0700

    Rewrite accuracy test

--- tests/spec/ext_framebuffer_multisample/accuracy.cpp
+++ tests/spec/ext_framebuffer_multisample/accuracy.cpp
@@ -23,33 +23,33 @@
 
 #include "piglit-util.h"
 
-const int NUM_HORIZ_TILES = 8;
-const int NUM_VERT_TILES = 8;
-const int NUM_TOTAL_TILES = NUM_HORIZ_TILES * NUM_VERT_TILES;
-const int TILE_SIZE = 32;
-const int UPSAMPLE_FACTOR = 16;
+const int pattern_width = 256;
+const int pattern_height = 256;
+const int SUPERSAMPLE_FACTOR = 16;
 
-int piglit_width = TILE_SIZE * NUM_HORIZ_TILES * 2;
-int piglit_height = TILE_SIZE * NUM_VERT_TILES;
+int piglit_width = 512;
+int piglit_height = 256;
 int piglit_window_mode = GLUT_RGBA | GLUT_ALPHA | GLUT_DOUBLE;
 
 class Fbo
 {
 public:
-	Fbo(bool multisampled, int width, int height);
+	void init(bool multisampled, int width, int height);
 	void set_viewport();
 
-	GLuint handle;
-	GLuint tex;
-
-private:
 	int width;
 	int height;
+	GLuint handle;
+	GLuint tex;
 };
 
-Fbo::Fbo(bool multisampled, int width, int height)
-	: tex(0), width(width), height(height)
+void
+Fbo::init(bool multisampled, int width, int height)
 {
+	this->tex = 0;
+	this->width = width;
+	this->height = height;
+
 	glGenFramebuffers(1, &handle);
 	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, handle);
 
@@ -99,83 +99,23 @@ Fbo::set_viewport()
 	glViewport(0, 0, width, height);
 }
 
-Fbo *multisample_fbo = NULL;
-Fbo *upsample_fbo = NULL;
-
-class DrawProg
-{
-public:
-	DrawProg();
-	void use();
-	void set_rotation(float rotation);
-
-private:
-	GLint prog;
-	GLint rotation_loc;
-};
-
-DrawProg::DrawProg()
-{
-	static const char *vert =
-		"#version 130\n"
-		"uniform float rotation;\n"
-		"in vec2 pos;\n"
-		"void main()\n"
-		"{\n"
-		"  vec2 pos2 = pos;\n"
-		"  pos2 = mat2(cos(rotation), sin(rotation),\n"
-                "              -sin(rotation), cos(rotation)) * pos2;\n"
-		"  gl_Position = vec4(pos2, 0.0, 1.0);\n"
-		"}\n";
-	static const char *frag =
-		"#version 130\n"
-		"void main()\n"
-		"{\n"
-		"  gl_FragColor = vec4(1.0);\n"
-		"}\n";
-
-	piglit_require_GLSL_version(130);
-	prog = piglit_CreateProgram();
-	GLint vs = piglit_compile_shader_text(GL_VERTEX_SHADER, vert);
-	piglit_AttachShader(prog, vs);
-	GLint fs = piglit_compile_shader_text(GL_FRAGMENT_SHADER, frag);
-	piglit_AttachShader(prog, fs);
-	piglit_BindAttribLocation(prog, 0, "pos");
-	piglit_LinkProgram(prog);
-	if (!piglit_link_check_status(prog)) {
-		piglit_report_result(PIGLIT_FAIL);
-	}
-	rotation_loc = piglit_GetUniformLocation(prog, "rotation");
-}
-
-void
-DrawProg::use()
-{
-	piglit_UseProgram(prog);
-}
-
-void
-DrawProg::set_rotation(float rotation)
-{
-	piglit_Uniform1f(rotation_loc, rotation);
-}
-
-DrawProg *draw_prog = NULL;
+Fbo multisample_fbo;
+Fbo supersample_fbo;
 
 class DownsampleProg
 {
 public:
-	DownsampleProg();
-	void use();
-	void set_samp(int texture_num);
+	void compile();
+	void run(const Fbo *src_fbo, int dstX0, int dstY0, int dstX1, int dstY1);
 
 private:
 	GLint prog;
-	GLint upsample_factor_loc;
-	GLint samp_loc;
+	GLuint vertex_buf;
+	GLuint vao;
 };
 
-DownsampleProg::DownsampleProg()
+void
+DownsampleProg::compile()
 {
 	static const char *vert =
 		"#version 130\n"
@@ -191,21 +131,22 @@ DownsampleProg::DownsampleProg()
 	static const char *frag =
 		"#version 130\n"
 		"uniform sampler2D samp;\n"
-		"uniform int upsample_factor;\n"
+		"uniform int supersample_factor;\n"
 		"in vec2 texCoordVarying;\n"
 		"void main()\n"
 		"{\n"
 		"  vec4 sum = vec4(0.0);\n"
 		"  ivec2 pixel = ivec2(texCoordVarying);\n"
-		"  for (int i = 0; i < upsample_factor; ++i) {\n"
-		"    for (int j = 0; j < upsample_factor; ++j) {\n"
+		"  for (int i = 0; i < supersample_factor; ++i) {\n"
+		"    for (int j = 0; j < supersample_factor; ++j) {\n"
 		"      sum += texelFetch(\n"
-		"          samp, pixel * upsample_factor + ivec2(i, j), 0);\n"
+		"          samp, pixel * supersample_factor + ivec2(i, j), 0);\n"
 		"    }\n"
 		"  }\n"
-		"  gl_FragColor = sum / (upsample_factor * upsample_factor);\n"
+		"  gl_FragColor = sum / (supersample_factor * supersample_factor);\n"
 		"}\n";
 
+	/* Compile program */
 	piglit_require_GLSL_version(130);
 	prog = piglit_CreateProgram();
 	GLint vs = piglit_compile_shader_text(GL_VERTEX_SHADER, vert);
@@ -218,228 +159,309 @@ DownsampleProg::DownsampleProg()
 	if (!piglit_link_check_status(prog)) {
 		piglit_report_result(PIGLIT_FAIL);
 	}
-	upsample_factor_loc = piglit_GetUniformLocation(prog, "upsample_factor");
-	samp_loc = piglit_GetUniformLocation(prog, "samp");
-}
 
-void
-DownsampleProg::use()
-{
+	/* Set up uniforms */
 	piglit_UseProgram(prog);
-	piglit_Uniform1i(upsample_factor_loc, UPSAMPLE_FACTOR);
-}
+	piglit_Uniform1i(piglit_GetUniformLocation(prog, "supersample_factor"),
+			 SUPERSAMPLE_FACTOR);
+	piglit_Uniform1i(piglit_GetUniformLocation(prog, "samp"), 0);
 
-void
-DownsampleProg::set_samp(int texture_num)
-{
-	piglit_Uniform1i(samp_loc, texture_num);
-}
+	/* Set up vertex input buffer */
+	glGenBuffers(1, &vertex_buf);
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_buf);
 
-DownsampleProg *downsample_prog = NULL;
-
-class TestShape
-{
-public:
-	TestShape(int tile_num);
-	void draw();
-	void draw_tile();
-	void draw_reference();
-
-private:
-	int x_tile;
-	int y_tile;
-	int y_tile_neg;
-	float v0;
-	float v1;
-	float rotation;
-};
-
-TestShape::TestShape(int tile_num)
-	: x_tile(tile_num % NUM_HORIZ_TILES),
-	  y_tile(tile_num / NUM_HORIZ_TILES),
-	  y_tile_neg(NUM_VERT_TILES - 1 - y_tile),
-	  v0(float(tile_num % (NUM_TOTAL_TILES/2)) / (NUM_TOTAL_TILES/2)),
-	  v1(1.0 - v0),
-	  rotation(M_PI * 2.0 * tile_num / NUM_TOTAL_TILES)
-{
-}
-
-void
-TestShape::draw()
-{
-	float vertices[4][2] = {
-		{ -0.4, -0.8 },
-		{  0.0,  0.8 },
-		{  0.4, -0.8 }
-	};
-
-	draw_prog->use();
-	draw_prog->set_rotation(rotation);
+	/* Set up vertex array object */
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
 	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(vertices[0]),
-			      &vertices);
-	glClear(GL_COLOR_BUFFER_BIT);
-	glDrawArrays(GL_TRIANGLES, 0, 3);
-}
-
-void
-TestShape::draw_tile()
-{
-	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, multisample_fbo->handle);
-	multisample_fbo->set_viewport();
-	draw();
-	glBindFramebuffer(GL_READ_FRAMEBUFFER, multisample_fbo->handle);
-	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
-	int x0 = TILE_SIZE * (x_tile + NUM_HORIZ_TILES);
-	int x1 = TILE_SIZE * (x_tile + NUM_HORIZ_TILES + 1);
-	int y0 = TILE_SIZE * y_tile_neg;
-	int y1 = TILE_SIZE * (y_tile_neg + 1);
-	glBlitFramebuffer(0, 0, TILE_SIZE, TILE_SIZE, x0, y0, x1, y1,
-			  GL_COLOR_BUFFER_BIT, GL_NEAREST);
-}
-
-void
-TestShape::draw_reference()
-{
-	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, upsample_fbo->handle);
-	upsample_fbo->set_viewport();
-	draw();
-	glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
-	glViewport(0, 0, piglit_width, piglit_height);
-	downsample_prog->use();
-	glActiveTexture(GL_TEXTURE0);
-	downsample_prog->set_samp(0);
-	glBindTexture(GL_TEXTURE_2D, upsample_fbo->tex);
-	float x0 = float(TILE_SIZE * x_tile) / piglit_width * 2.0 - 1.0;
-	float x1 = float(TILE_SIZE * (x_tile + 1)) / piglit_width * 2.0 - 1.0;
-	float y0 = float(TILE_SIZE * y_tile_neg) / piglit_height * 2.0 - 1.0;
-	float y1 = float(TILE_SIZE * (y_tile_neg + 1)) / piglit_height * 2.0 - 1.0;
-	float vertices[4][2] = {
-		{ x0, y0 },
-		{ x0, y1 },
-		{ x1, y1 },
-		{ x1, y0 }
-	};
-	float texCoords[4][2] = {
-		{ 0, 0 },
-		{ 0, TILE_SIZE },
-		{ TILE_SIZE, TILE_SIZE },
-		{ TILE_SIZE, 0 }
-	};
-	glEnableVertexAttribArray(0);
-	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, sizeof(vertices[0]),
-			      &vertices);
+	glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 4*sizeof(float),
+			      (void *) 0);
 	glEnableVertexAttribArray(1);
-	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, sizeof(texCoords[0]),
-			      &texCoords);
+	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4*sizeof(float),
+			      (void *) (2*sizeof(float)));
+}
+
+void
+DownsampleProg::run(const Fbo *src_fbo, int dstX0, int dstY0, int dstX1, int dstY1)
+{
+	float x0 = dstX0 * 2.0 / piglit_width - 1.0;
+	float x1 = dstX1 * 2.0 / piglit_width - 1.0;
+	float y0 = dstY0 * 2.0 / piglit_height - 1.0;
+	float y1 = dstY1 * 2.0 / piglit_height - 1.0;
+	float w = src_fbo->width / SUPERSAMPLE_FACTOR;
+	float h = src_fbo->height / SUPERSAMPLE_FACTOR;
+
+	glActiveTexture(GL_TEXTURE0);
+	glBindTexture(GL_TEXTURE_2D, src_fbo->tex);
+
+	piglit_UseProgram(prog);
+	glBindVertexArray(vao);
+
+	float vertex_data[4][4] = {
+		{ x0, y0, 0, 0 },
+		{ x0, y1, 0, h },
+		{ x1, y1, w, h },
+		{ x1, y0, w, 0 }
+	};
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_buf);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertex_data), vertex_data,
+		     GL_STREAM_DRAW);
+
+	/* TODO: move to VAO */
 	unsigned int indices[6] = { 0, 1, 2, 0, 2, 3 };
 	glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, indices);
 }
 
-void
-draw_pattern()
-{
-	for (int i = 0; i < NUM_TOTAL_TILES; ++i) {
-		TestShape(i).draw_reference();
-		TestShape(i).draw_tile();
-	}
-}
+DownsampleProg downsample_prog;
 
-class Stat
+class TilingProjMatrix
 {
 public:
-	Stat();
+	TilingProjMatrix(int tile_width, int tile_height,
+			 int x_offset, int y_offset);
 
-	void record(float error)
-	{
-		++count;
-		sum_squared_error += error * error;
-	}
-
-	void summarize();
-
-private:
-	int count;
-	double sum_squared_error;
+	float values[4][4];
 };
 
-Stat::Stat()
-	: count(0), sum_squared_error(0.0)
+TilingProjMatrix::TilingProjMatrix(int tile_width, int tile_height,
+				   int x_offset, int y_offset)
 {
+	/* Need a projection matrix such that:
+	 * xc = ((xe + 1) * pattern_width/2 - x_offset) * 2/tile_width - 1
+	 * yc = ((ye + 1) * pattern_height/2 - y_offset) * 2/tile_height - 1
+	 * zc = ze
+	 * wc = we = 1.0
+	 *
+	 * Therefore
+	 * xc = pattern_width / tile_width * xe
+	 *    + pattern_width / tile_width - x_offset * 2 / tile_width - 1
+	 * yc = pattern_height / tile_height * ye
+	 *    + pattern_height / tile_height - y_offset * 2 / tile_height - 1
+	 * zc = ze
+	 * wc = we = 1.0
+	 */
+	memset(values, 0, sizeof(values));
+	values[0][0] = float(pattern_width) / tile_width;
+	values[0][3] = values[0][0] - x_offset * 2.0 / tile_width - 1.0;
+	values[1][1] = float(pattern_height) / tile_height;
+	values[1][3] = values[1][1] - y_offset * 2.0 / tile_height - 1.0;
+	values[2][2] = 1.0;
+	values[3][3] = 1.0;
+}
+
+class TestPattern
+{
+public:
+	virtual void compile() = 0;
+	virtual void draw(const TilingProjMatrix *proj) = 0;
+};
+
+class Triangles : public TestPattern
+{
+public:
+	virtual void compile();
+	virtual void draw(const TilingProjMatrix *proj);
+
+private:
+	GLint prog;
+	GLuint vertex_buf;
+	GLuint vao;
+	GLint proj_loc;
+	int num_tris;
+};
+
+void Triangles::compile()
+{
+	/* Triangle coords within (-1,-1) to (1,1) rect */
+	static const float pos_within_tri[][2] = {
+		{ -0.5, -1.0 },
+		{  0.0,  1.0 },
+		{  0.5, -1.0 }
+	};
+
+	/* Number of triangle instances across (and down) */
+	int tris_across = 8;
+
+	/* Total number of triangles drawn */
+	num_tris = tris_across * tris_across;
+
+	/* Scaling factor uniformly applied to triangle coords */
+	float tri_scale = 0.8 / tris_across;
+
+	/* Amount each triangle should be rotated compared to prev */
+	float rotation_delta = M_PI * 2.0 / num_tris;
+
+	/* Final scaling factor */
+	float final_scale = 0.95;
+
+	static const char *vert =
+		"#version 130\n"
+		"#extension GL_ARB_draw_instanced: require\n"
+		"in vec2 pos_within_tri;\n"
+		"uniform float tri_scale;\n"
+		"uniform float rotation_delta;\n"
+		"uniform int tris_across;\n"
+		"uniform float final_scale;\n"
+		"uniform mat4 proj;\n"
+		"\n"
+		"void main()\n"
+		"{\n"
+		"  vec2 pos = tri_scale * pos_within_tri;\n"
+		"  float rotation = rotation_delta * gl_InstanceID;\n"
+		"  pos = mat2(cos(rotation), sin(rotation),\n"
+		"             -sin(rotation), cos(rotation)) * pos;\n"
+		"  int i = gl_InstanceID % tris_across;\n"
+		"  int j = tris_across - 1 - gl_InstanceID / tris_across;\n"
+		"  pos += (vec2(i, j) * 2.0 + 1.0) / tris_across - 1.0;\n"
+		"  pos *= final_scale;\n"
+		"  gl_Position = proj * vec4(pos, 0.0, 1.0);\n"
+		"}\n";
+
+	static const char *frag =
+		"#version 130\n"
+		"void main()\n"
+		"{\n"
+		"  gl_FragColor = vec4(1.0);\n"
+		"}\n";
+
+	/* Compile program */
+	piglit_require_GLSL_version(130);
+	piglit_require_extension("GL_ARB_draw_instanced");
+	prog = piglit_CreateProgram();
+	GLint vs = piglit_compile_shader_text(GL_VERTEX_SHADER, vert);
+	piglit_AttachShader(prog, vs);
+	GLint fs = piglit_compile_shader_text(GL_FRAGMENT_SHADER, frag);
+	piglit_AttachShader(prog, fs);
+	piglit_BindAttribLocation(prog, 0, "pos_within_tri");
+	piglit_LinkProgram(prog);
+	if (!piglit_link_check_status(prog)) {
+		piglit_report_result(PIGLIT_FAIL);
+	}
+
+	/* Set up uniforms */
+	piglit_UseProgram(prog);
+	piglit_Uniform1f(piglit_GetUniformLocation(prog, "tri_scale"),
+			 tri_scale);
+	piglit_Uniform1f(piglit_GetUniformLocation(prog, "rotation_delta"),
+			 rotation_delta);
+	piglit_Uniform1i(piglit_GetUniformLocation(prog, "tris_across"),
+			 tris_across);
+	piglit_Uniform1f(piglit_GetUniformLocation(prog, "final_scale"),
+			 final_scale);
+	proj_loc = piglit_GetUniformLocation(prog, "proj");
+
+	/* Set up vertex input buffer */
+	glGenBuffers(1, &vertex_buf);
+	glBindBuffer(GL_ARRAY_BUFFER, vertex_buf);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(pos_within_tri), pos_within_tri,
+		     GL_STATIC_DRAW);
+
+	/* Set up vertex array object */
+	glGenVertexArrays(1, &vao);
+	glBindVertexArray(vao);
+	glEnableVertexAttribArray(0);
+	glVertexAttribPointer(0, ARRAY_SIZE(pos_within_tri[0]), GL_FLOAT,
+			      GL_FALSE, sizeof(pos_within_tri[0]), (void *) 0);
+}
+
+void Triangles::draw(const TilingProjMatrix *proj)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	piglit_UseProgram(prog);
+	piglit_UniformMatrix4fv(proj_loc, 1, GL_TRUE, &proj->values[0][0]);
+	glBindVertexArray(vao);
+	glDrawArraysInstanced(GL_TRIANGLES, 0, 3, num_tris);
+}
+
+Triangles triangles;
+
+void
+piglit_init(int argc, char **argv)
+{
+	/* TODO: choose whether to test small multisample_fbo by command line arg */
+	multisample_fbo.init(true /* multisampled */,
+			     pattern_width / 4, pattern_height / 4);
+	supersample_fbo.init(false /* multisampled */,
+			     1024, 1024);
+
+	triangles.compile();
+	downsample_prog.compile();
 }
 
 void
-Stat::summarize()
+draw_test_image(TestPattern *pattern)
 {
-	printf("  count = %d\n", count);
-	if (count != 0) {
-		if (sum_squared_error != 0.0) {
-			printf("  RMS error = %f\n",
-			       sqrt(sum_squared_error / count));
-		} else {
-			printf("  Perfect output\n");
+	int num_h_tiles = pattern_width / multisample_fbo.width;
+	int num_v_tiles = pattern_height / multisample_fbo.height;
+	for (int h = 0; h < num_h_tiles; ++h) {
+		for (int v = 0; v < num_v_tiles; ++v) {
+			glBindFramebuffer(GL_DRAW_FRAMEBUFFER,
+					  multisample_fbo.handle);
+			multisample_fbo.set_viewport();
+			int x_offset = h * multisample_fbo.width;
+			int y_offset = v * multisample_fbo.height;
+			TilingProjMatrix proj(multisample_fbo.width,
+					      multisample_fbo.height,
+					      x_offset,
+					      y_offset);
+			pattern->draw(&proj);
+			// TODO: convert to color buffer if needed
+			glBindFramebuffer(GL_READ_FRAMEBUFFER, multisample_fbo.handle);
+			glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+			glViewport(0, 0, piglit_width, piglit_height);
+			glBlitFramebuffer(0, 0, multisample_fbo.width,
+					  multisample_fbo.height,
+					  x_offset, y_offset,
+					  x_offset + multisample_fbo.width,
+					  y_offset + multisample_fbo.height,
+					  GL_COLOR_BUFFER_BIT, GL_NEAREST);
+			// TODO: convert to color buffer if needed
 		}
 	}
 }
 
 void
-measure_accuracy()
+draw_reference_image(TestPattern *pattern)
 {
-	glBindFramebuffer(GL_READ_FRAMEBUFFER, 0);
-	int width = TILE_SIZE * NUM_HORIZ_TILES;
-	int height = TILE_SIZE * NUM_VERT_TILES;
-
-	float *reference_data = new float[width * height * 4];
-	glReadPixels(0, 0, width, height, GL_RGBA, GL_FLOAT, reference_data);
-
-	float *test_data = new float[width * height * 4];
-	glReadPixels(width, 0, width, height, GL_RGBA, GL_FLOAT, test_data);
-
-	Stat unlit_stats;
-	Stat partially_lit_stats;
-	Stat totally_lit_stats;
-	for (int y = 0; y < height; ++y) {
-		for (int x = 0; x < width; ++x) {
-			float ref = reference_data[y*height + x];
-			float test = test_data[y*height + x];
-			if (ref <= 0.0)
-				unlit_stats.record(test - ref);
-			else if (ref >= 1.0)
-				totally_lit_stats.record(test - ref);
-			else
-				partially_lit_stats.record(test - ref);
+	int downsampled_width = supersample_fbo.width / SUPERSAMPLE_FACTOR;
+	int downsampled_height = supersample_fbo.height / SUPERSAMPLE_FACTOR;
+	int num_h_tiles = pattern_width / downsampled_width;
+	int num_v_tiles = pattern_height / downsampled_height;
+	for (int h = 0; h < num_h_tiles; ++h) {
+		for (int v = 0; v < num_v_tiles; ++v) {
+			glBindFramebuffer(GL_DRAW_FRAMEBUFFER,
+					  supersample_fbo.handle);
+			supersample_fbo.set_viewport();
+			int x_offset = h * downsampled_width;
+			int y_offset = v * downsampled_height;
+			TilingProjMatrix proj(downsampled_width,
+					      downsampled_height,
+					      x_offset,
+					      y_offset);
+			pattern->draw(&proj);
+			// TODO: convert to color buffer if needed
+			glBindFramebuffer(GL_DRAW_FRAMEBUFFER, 0);
+			glViewport(0, 0, piglit_width, piglit_height);
+			downsample_prog.run(&supersample_fbo,
+					    x_offset + pattern_width,
+					    y_offset,
+					    x_offset + pattern_width
+					    + downsampled_width,
+					    y_offset + downsampled_height);
 		}
 	}
-
-	printf("Pixels that should be unlit\n");
-	unlit_stats.summarize();
-	printf("Pixels that should be totally lit\n");
-	totally_lit_stats.summarize();
-	printf("Pixels that should be partially lit\n");
-	partially_lit_stats.summarize();
 }
 
 enum piglit_result
 piglit_display()
 {
-	draw_pattern();
-	measure_accuracy();
+	draw_test_image(&triangles);
+	draw_reference_image(&triangles);
+
+	// TODO: measure accuracy.
 
 	piglit_present_results();
 
 	return PIGLIT_PASS;
-}
-
-void
-piglit_init(int argc, char **argv)
-{
-	multisample_fbo = new Fbo(true, /* multisampled */
-				  TILE_SIZE, TILE_SIZE);
-	upsample_fbo = new Fbo(false, /* multisampled */
-			       TILE_SIZE * UPSAMPLE_FACTOR,
-			       TILE_SIZE * UPSAMPLE_FACTOR);
-
-	downsample_prog = new DownsampleProg();
-	draw_prog = new DrawProg();
-	draw_prog->use();
 }
