371efa45b823c11166a3e8ac8f53086a0508a7b7
Pauli Nieminen <pauli.nieminen@linux.intel.com>
2012-05-21 21:08:36 +0300

    texline: Replace glut with piglit-util and glut_wrap.h
    Signed-off-by: Pauli Nieminen <pauli.nieminen@linux.intel.com>

--- tests/mesa/tests/texline.c
+++ tests/mesa/tests/texline.c
@@ -12,7 +12,8 @@
 #include <stdlib.h>
 #include <string.h>
 #include <math.h>
-#include <GL/glut.h>
+#include "piglit-util.h"
+#include <piglit/glut_wrap.h>
 #include "../util/readtex.h"
 #include "../util/writeimg.h"
 
